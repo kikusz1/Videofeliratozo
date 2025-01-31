5c1c75dffebfe5641b09c4b3ed9088afd2dd6c47
kikusz1 <66381334+kikusz1@users.noreply.github.com>
2025-01-31 19:21:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const imageContainer = document.querySelector('.image-container');
 let isDragging = false;
 let startX = 0;
 let scrollLeft = 0;
+let throttled = false;
 
 imageContainer.addEventListener('mousedown', (e) => {
     isDragging = true;
@@ -18,9 +19,15 @@ imageContainer.addEventListener('mouseup', () => {
 });
 
 imageContainer.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
+    if (!isDragging || throttled) return;
     e.preventDefault();
+
+    throttled = true;
     const x = e.pageX - imageContainer.offsetLeft;
-    const walk = (x - startX) * 2; // Szorzó a görgetés sebességének beállításához
+    const walk = (x - startX) * 2;
     imageContainer.scrollLeft = scrollLeft - walk;
+
+    setTimeout(() => {
+        throttled = false;
+    }, 16); // 16ms késleltetés (kb. 60 FPS)
 });
