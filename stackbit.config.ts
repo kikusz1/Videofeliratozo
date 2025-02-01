e34c6da03f02cfdb651851f14fe1145382360c5d
kikusz1 <66381334+kikusz1@users.noreply.github.com>
2025-02-01 17:11:34 +0100

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -11,7 +11,7 @@ export default defineStackbitConfig({
           name: "Page", // Modell neve
           type: "page", // Ez egy oldalmodell
           urlPath: "https://videofeliratozo.netlify.app/", // Az oldal URL-je
-          filePath: "content/pages/{slug}.json", // A fájl elérési útvonala
+          filePath: "content/pages/package.json", // A fájl elérési útvonala
           fields: [
             { name: "title", type: "string", required: true }, // Cím mező
             { name: "content", type: "markdown", required: true } // Tartalom mező
