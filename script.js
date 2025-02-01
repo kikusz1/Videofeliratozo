e531ca53105f7265443d044bc4534e0204452f4b
kikusz1 <66381334+kikusz1@users.noreply.github.com>
2025-02-01 15:48:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,37 +9,3 @@ function scrollImages(direction) {
         imageContainer.scrollBy({ left: scrollAmount, behavior: 'smooth' });
     }
 }
-
-// Töröltük a "megfogásos" görgetéshez kapcsolódó részeket
-// Az alábbi kódok törlésre kerültek:
-/*
-let isDragging = false;
-let startX = 0;
-let scrollLeft = 0;
-let throttled = false;
-
-imageContainer.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    startX = e.pageX - imageContainer.offsetLeft;
-    scrollLeft = imageContainer.scrollLeft;
-});
-
-imageContainer.addEventListener('mouseleave', () => {
-    isDragging = false;
-});
-
-imageContainer.addEventListener('mouseup', () => {
-    isDragging = false;
-});
-
-imageContainer.addEventListener('mousemove', (e) => {
-    if (!isDragging || throttled) return;
-    throttled = true;
-    const x = e.pageX - imageContainer.offsetLeft;
-    const walk = (x - startX) * 2;
-    imageContainer.scrollLeft = scrollLeft - walk;
-    setTimeout(() => {
-        throttled = false;
-    }, 16); // 16ms késleltetés (kb. 60 FPS)
-});
-*/
