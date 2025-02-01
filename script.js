9ea58ad1e0a49e2f81d39dc24b954049b03e4201
kikusz1 <66381334+kikusz1@users.noreply.github.com>
2025-02-01 15:33:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ let startX = 0;
 let scrollLeft = 0;
 let throttled = false;
 
+function scrollImages(direction) {
+    const imageContainer = document.querySelector('.image-container');
+    const scrollAmount = 300; // Görgés mértéke (pixelekben)
+
+    if (direction === 'left') {
+        imageContainer.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    } else if (direction === 'right') {
+        imageContainer.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    }
+}
+
 imageContainer.addEventListener('mousedown', (e) => {
     isDragging = true;
     startX = e.pageX - imageContainer.offsetLeft;
